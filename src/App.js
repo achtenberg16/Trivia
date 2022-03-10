8456cf36dbc8d5eee38bf6b09d2cbcce72d49d9e
achtenberg167 <achtenberg16@gmail.com.br>
2022-03-10 15:28:35 -0300

    feat: adicionado button settings, logica de redirect para ele e componente settings

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" render={ (props) => <Login { ...props } /> } />
+        <Route path="/settings" component={ Settings } />
       </Switch>
     </div>
   );

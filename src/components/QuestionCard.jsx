aeb110cdb001af03791e4a6ab72e70cfde07bc02
achtenberg167 <achtenberg16@gmail.com.br>
2022-03-11 13:45:02 -0300

    feat: implementado um timer de 30 seg

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import shuffleArray from '../helpers';
 
+const TIMER_TO_RESPONDE = 30;
+const ONE_SECOND = 1000;
+
 class QuestionCard extends React.Component {
   constructor() {
     super();
     this.state = {
       answers: [],
       responseIntervalIsOver: false,
+      timer: TIMER_TO_RESPONDE,
     };
   }
 
   componentDidMount() {
     this.shuffleAnswers();
+    this.initTimer();
   }
 
   componentDidUpdate(prevProps) {
@@ -20,6 +25,19 @@ class QuestionCard extends React.Component {
     const { questionsIndex: indexActual } = this.state;
     if (questionsIndex !== indexActual) this.shuffleAnswers();
   }
+
+  initTimer = () => {
+    this.timer = setInterval(() => {
+      const { timer } = this.state;
+      if (timer === 0) { this.timeOver(); return true; }
+      this.setState((prevstate) => ({ timer: prevstate.timer - 1 }));
+    }, ONE_SECOND);
+  }
+
+timeOver = () => {
+  clearInterval(this.timer);
+  this.setState({ responseIntervalIsOver: true });
+}
 
   shuffleAnswers = () => {
     const { questionActual, questionsIndex } = this.props;
@@ -39,7 +57,7 @@ class QuestionCard extends React.Component {
   });
 
   render() {
-    const { answers, responseIntervalIsOver } = this.state;
+    const { answers, responseIntervalIsOver, timer } = this.state;
     const { questionActual } = this.props;
     const { correct_answer: correct } = questionActual;
 
@@ -51,7 +69,7 @@ class QuestionCard extends React.Component {
 
     return (
       <div>
-
+        <h1>{timer}</h1>
         <h2 data-testid="question-category">{ questionActual.category }</h2>
         <h3 data-testid="question-text">{ questionActual.question }</h3>
 

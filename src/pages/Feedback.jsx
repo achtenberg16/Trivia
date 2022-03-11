d64e6cd73d1f40870e42e1f4d8524fdd445a54e2
Isabela Assunção <isa.btssp2015@gmail.com>
2022-03-11 17:05:53 -0300

    feat: requisito 14 e 15 - Mostra quantidade de acertos e redireciona para tela inicial caso a pessoa queira jogar novamente

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { object, PropTypes } from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends React.Component {
+  handleButton = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
-    const { assertions } = this.props;
+    const { assertions, score } = this.props;
     const min = 3;
     return (
       <div className="feedback">
         <Header />
         {
           assertions < min ? (
-            <h3 className="fb-message" data-testid="feedback-text">Could be better</h3>
-          ) : <h3 className="fb-message" data-testid="feedback-text">Well Done!</h3>
+            <h2 className="fb-message" data-testid="feedback-text">Could be better</h2>
+          ) : <h2 className="fb-message" data-testid="feedback-text">Well Done!</h2>
         }
+        <h3 data-testid="feedback-total-score" className="fb-score">
+          { `Your score: ${score}` }
+        </h3>
+        <p data-testid="feedback-total-question" className="fb-assertions">
+          { `You got ${assertions} questions right` }
+        </p>
+        <button
+          data-testid="btn-play-again"
+          type="button"
+          className="fb-button"
+          onClick={ this.handleButton }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
 
 Feedback.propTypes = {
-  assertions: PropTypes.number.isRequired,
-};
+  assertions: PropTypes.number,
+  score: PropTypes.number,
+  history: object,
+}.isRequired;
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 // state.player.assertions

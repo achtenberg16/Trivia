f147022aeb8dc36bcd90517bb193b62282da8b11
achtenberg167 <achtenberg16@gmail.com.br>
2022-03-11 10:27:32 -0300

    refactor: altera o componente questions card para ele receber a questao atual do array e nao da store

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,52 +1,52 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import shuffleArray from '../helpers';
 
 class QuestionCard extends React.Component {
   render() {
-    const { questions: { questions, questionsIndex } } = this.props;
+    const { questionActual } = this.props;
+    const { incorrect_answers: incorrects, correct_answer: correct } = questionActual;
+
     let wrongIndex = 0;
     const updateIndex = () => {
       wrongIndex += 1;
     };
-    let questionActual = '';
-    if (questions.length > 0) {
-      questionActual = questions[questionsIndex];
-      const { incorrect_answers, correct_answer } = questionActual;
-      const answers = shuffleArray([...incorrect_answers, correct_answer]);
-      return (
-        <div>
-          <h2 data-testid="question-category">{ questionActual.category }</h2>
-          <h3 data-testid="question-text">{ questionActual.question }</h3>
-          <div data-testid="answer-options">
-            { answers.map((asw) => {
-              let testID = 'correct-answer';
-              if (asw !== correct_answer) {
-                testID = `wrong-answer-${wrongIndex}`;
-                updateIndex();
-              }
-              return (
-                <button
-                  data-testid={ testID }
-                  key={ asw }
-                  type="button"
-                >
-                  {asw}
-                </button>
-              );
-            })}
-          </div>
-        </div>
-      );
-    }
+
+    const answers = shuffleArray([...incorrects, correct]);
+
     return (
-      <div />
+      <div>
+
+        <h2 data-testid="question-category">{ questionActual.category }</h2>
+        <h3 data-testid="question-text">{ questionActual.question }</h3>
+
+        <div data-testid="answer-options">
+          { answers.map((asw) => {
+            let testID = 'correct-answer';
+            if (asw !== correct) {
+              testID = `wrong-answer-${wrongIndex}`;
+              updateIndex();
+            }
+
+            return (
+              <button
+                data-testid={ testID }
+                key={ asw }
+                type="button"
+              >
+                {asw}
+              </button>
+            );
+          })}
+        </div>
+
+      </div>
     );
   }
 }
 
-const mapStateToProps = ({ questions }) => ({
-  questions,
-});
+QuestionCard.propTypes = {
+  questionActual: PropTypes.object,
+}.isRequired;
 
-export default connect(mapStateToProps)(QuestionCard);
+export default QuestionCard;

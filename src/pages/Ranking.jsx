8ce014e9ebd441aa8fe0a94779ca4a1438c05c49
Isabela Assunção <isa.btssp2015@gmail.com>
2022-03-13 01:35:12 -0300

    ft: css tela ranking

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getItemLocalStorage } from '../services/LocalStorage';
+import '../css/Ranking.css';
 
 class Ranking extends React.Component {
   handleButton = () => {
@@ -12,14 +13,14 @@ class Ranking extends React.Component {
   render() {
     const ranking = getItemLocalStorage('ranking') || [];
     return (
-      <div>
+      <div className="ranking">
         <h1 className="r-title" data-testid="ranking-title">RANKING</h1>
-        <ol>
+        <ol className="r-list">
           {ranking.map(({ name, score, picture }, index) => (
-            <li key={ name + picture + score }>
-              <img src={ picture } alt="user-profile" />
-              <p data-testid={ `player-name${index}` }>{name}</p>
-              <p data-test-id={ `player-score${index}` }>{score}</p>
+            <li className="r-item" key={ name + picture + score }>
+              <img className="r-img" src={ picture } alt="user-profile" />
+              <p className="r-name" data-testid={ `player-name${index}` }>{name}</p>
+              <p className="r-score" data-test-id={ `player-score${index}` }>{score}</p>
             </li>
           ))}
         </ol>
